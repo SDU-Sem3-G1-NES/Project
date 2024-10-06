1481d2d3df300af39323e38478196aa7a9aceee0
Zuhao <yalin1998@hotmail.com>
2024-10-07 00:46:43 +0200

    Simple IUser interface with example user type usage

---
+++ IUser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public interface IUser
+{
+	int UserID { get; set; }
+	string Name { get; set; }
+	string Email { get; set; }
+	string Address { get; set; }
+}
+public class Admin : IUser
+{
+	public int UserID { get; set; }
+	public string Name { get; set; }
+	public string Email { get; set; }
+	public string Address { get; set; }
+
+	public bool ManageUsers { get; set; }
+}
+
+public class Employee : IUser
+{
+	public int UserID { get; set; }
+	public string Name { get; set; }
+	public string Email { get; set; }
+	public string Address { get; set; }
+
+	public string Department { get; set; }
+}
