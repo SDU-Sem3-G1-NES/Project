da8401d99062ec337d7ebb64f2af9fc1c5421799
Zuhao Zhang <zuzha23@student.sdu.dk>
2024-10-07 12:48:43 +0200

    -Deleted excess file

--- Employee.cs
+++
@@ -1,9 +0,0 @@
-using FamicomBackend;
-public class Employee : IUser
-{
-	public int UserID { get; set; }
-	public string Name { get; set; }
-	public string Email { get; set; }
-	IUserPermissions Permissions { get; set; }
-    public string Department { get; set; }
-}
