6c9bfc70d1760a3c6662ed59c4a8aa2059db0ebc
Simonas Tarasauskas <simonas.tarasauskas@gmail.com>
2024-10-07 11:23:58 +0200

    NES-7 better naming and cleanup

--- Scheduling/ISchedule.cs
+++ Scheduling/ISchedule.cs
@@ -4,10 +4,10 @@ namespace Scheduling
 {
     public interface ISchedule
     {
-        int GUID { get; set; } // Schedule ID needed for cancellation.
-        string CronWhen { get; set; } // Cron expression to be used for task scheduling. With Hangfire? Bad naming?
-        int Height { get; set; } // Table height to be set.
-        void Set(int ScheduleGUID, string cronWhen, int TableGUID, int height);
+        int GUID { get; private set; }
+        DateTime TimeStamp { get; private set; }
+        int Height { get; private set; }
+        void Set(int ScheduleGUID, DateTime timeStamp, int TableGUID, int height);
         void Cancel(int GUID);
     }
 }
