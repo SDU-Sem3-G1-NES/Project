d6c21a2a51a77377c966a0cca32da6fd12cb3617
Nick <niklas.braun30419@gmail.com>
2024-10-24 20:46:26 +0200

    Periodically read rotary encoder states

--- EmbeddedSolutionArduino/src/main.cpp
+++ EmbeddedSolutionArduino/src/main.cpp
@@ -1,37 +1,42 @@
-#include <Arduino.h>
+#include<Arduino.h>
 
-// Potentiometer
-#define outputA 6
-#define outputB 7
-#define outputButton 8
+// Define rotary encoder pins
+#define ROT_A 2 // Left Turn
+#define ROT_B 3 // RIght Turn
+#define ROT_C 4 // Push Button
 
-int counter = 0; 
-int aState;
-int aLastState;  
+unsigned long _lastIncReadTime = micros(); 
+unsigned long _lastDecReadTime = micros(); 
+int _pauseLength = 25000;
+int _fastIncrement = 10;
+
+void read_rotary_encoder(); // Function declaration
 
 void setup() {
-  pinMode (outputA,INPUT);
-  pinMode (outputB,INPUT);
-  pinMode (outputButton,INPUT);
 
+  // Set encoder pins and attach interrupts
+  pinMode(ROT_A, INPUT_PULLUP);
+  pinMode(ROT_B, INPUT_PULLUP);
+  pinMode(ROT_C, INPUT_PULLUP);
+  //attachInterrupt(digitalPinToInterrupt(ROT_A), read_rotary_encoder, CHANGE);
+  //attachInterrupt(digitalPinToInterrupt(ROT_B), read_rotary_encoder, CHANGE);
 
-  Serial.begin (9600);
-  // Reads the initial state of the outputA
-  aLastState = digitalRead(outputA);   
+  // Start the serial monitor to show output
+  Serial.begin(115200);
 }
 
 void loop() {
-  aState = digitalRead(outputA); // Reads the "current" state of the outputA
-  // If the previous and the current state of the outputA are different, that means a Pulse has occured
-  if (aState != aLastState){     
-    // If the outputB state is different to the outputA state, that means the encoder is rotating clockwise
-    if (digitalRead(outputB) != aState) { 
-      counter ++;
-    } else {
-      counter --;
-    }
-    Serial.print("Position: ");
-    Serial.println(counter);
-  } 
-  aLastState = aState; // Updates the previous state of the outputA with the current state
+  delay(1000);
+  read_rotary_encoder();
 }
+
+void read_rotary_encoder() {
+  //static uint8_t old_AB = 3;  // Lookup table index
+  //static int8_t encval = 0;   // Encoder value  
+  //static const int8_t enc_states[]  = {0,-1,1,0,1,0,0,-1,-1,0,0,1,0,1,-1,0}; // Lookup table
+
+  //Serial.println("\nRotary Encoder Interrupt Triggered");
+  Serial.print("ROT_A: " + String(digitalRead(ROT_A)) + "\n");
+  Serial.print("ROT_B: " + String(digitalRead(ROT_B)) + "\n");
+  Serial.print("ROT_C: " + String(digitalRead(ROT_C)) + "\n");
+} 
