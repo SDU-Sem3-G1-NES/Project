1b4417972e4828d263bb3d143bdf830f850b659b
Nick <niklas.braun30419@gmail.com>
2024-10-25 08:46:04 +0200

    Implemented functioning button presses

--- EmbeddedSolutionArduino/src/main.cpp
+++ EmbeddedSolutionArduino/src/main.cpp
@@ -1,12 +1,13 @@
 #include<Arduino.h>
 
 // Define rotary encoder pins
-#define ROT_A 11 // Left Turn
-#define ROT_B 12 // RIght Turn
-#define ROT_C 13 // Push Button
+#define ROT_A 8 // Left Turn
+#define ROT_B 9 // Right Turn
+#define ROT_C 10 // Push Button
 
 volatile bool rotary_encoder_states[2] = {0, 0};
 volatile int rotary_encoder_position = 0;
+volatile bool ROT_C_LAST_STATE = 1;
 
 void read_rotary_encoder(); // Function declaration
 
@@ -15,8 +16,9 @@ void setup() {
   // Set encoder pins and attach interrupts
   pinMode(ROT_A, INPUT_PULLUP);
   pinMode(ROT_B, INPUT_PULLUP);
+  pinMode(ROT_C, INPUT_PULLUP);
   PCICR |= B00000001; // Enable Port B interrupt
-  PCMSK0 |= B00111000; // Enable interrupt on pins 11, 12, 13
+  PCMSK0 |= B00000111; // Enable interrupt on pins 11, 12, 13
 
   // Start the serial monitor to show output
   Serial.begin(115200);
@@ -29,10 +31,28 @@ void loop() {
 
 ISR(PCINT0_vect) {
   read_rotary_encoder();
+  //Serial.println(String(digitalRead(ROT_A)) + " " + String(digitalRead(ROT_B)) + " " + String(digitalRead(ROT_C)));
 }
 
 void read_rotary_encoder() {
-  //Serial.println(String(digitalRead(ROT_A)) + " " + String(digitalRead(ROT_B)));
+  switch(ROT_C_LAST_STATE) 
+  {
+    case 1:
+      if(digitalRead(ROT_C) == 0) {
+        Serial.println("Button Pressed");
+        ROT_C_LAST_STATE = 0;
+        return;
+      }
+      break;
+    case 0:
+      if(digitalRead(ROT_C) == 1) {
+        ROT_C_LAST_STATE = 1;
+      }
+      break;
+      
+    default:
+      break;
+  }
 
   bool ROT_A_STATE = digitalRead(ROT_A);
   bool ROT_B_STATE = digitalRead(ROT_B);
