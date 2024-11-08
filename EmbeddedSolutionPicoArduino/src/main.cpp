6089ee3f59c565e4d8c525ad8a09b910fcccf5d4
Nick <niklas.braun30419@gmail.com>
2024-11-08 12:51:10 +0100

    Add Display Driver

--- EmbeddedSolutionPicoArduino/src/main.cpp
+++ EmbeddedSolutionPicoArduino/src/main.cpp
@@ -1,32 +1,38 @@
-#include<Arduino.h>
+#include <Arduino.h>
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7789.h>
 #include "rotary_encoder.h"
 
-#define ROT_A 9
-#define ROT_B 10
-#define ROT_C 11
+
+
+// Rotary Encoder Definitions
+#define ROT_A 22
+#define ROT_B 21
+#define ROT_C 20
 rotary_encoder::input rotary_encoder_input(ROT_A, ROT_B, ROT_C);
 
 void pot_interrupt() {
   rotary_encoder_input.read_rotary_encoder();
 }
 
-void setup() {
+#define TFT_DC 14
+#define TFT_RST 12
+#define TFT_CS 13
+#define TFT_MOSI 11
+#define TFT_LED 15
+#define TFT_SCLK 10
 
-}
-
-void loop() {
-
-}
-
+float p = 3.1415926;
+void tft_test();
+Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
 
 // Setup1 is used to use the second CPU core
-void setup1() {
+void setup() {
   // Debug Probe
   Serial1.setRX(1);
   Serial1.setTX(0);
-  Serial1.begin(115200);
-
-
+  Serial1.begin(250000);
 
   pinMode(ROT_A, INPUT_PULLUP);
   pinMode(ROT_B, INPUT_PULLUP);
@@ -36,10 +42,22 @@ void setup1() {
   attachInterrupt(digitalPinToInterrupt(ROT_B), pot_interrupt, CHANGE);
   attachInterrupt(digitalPinToInterrupt(ROT_C), pot_interrupt, CHANGE);
 
-  Serial.begin(115200);
+  // Display
+  pinMode(TFT_LED, OUTPUT);
+  digitalWrite(TFT_LED, HIGH);
+
+  tft.init(240, 320);
+  tft.invertDisplay(false);
+  tft.fillScreen(ST77XX_RED);
+  delay(500);
+  tft.fillScreen(ST77XX_GREEN);
+  delay(500);
+  tft.fillScreen(ST77XX_BLUE);
 }
 
-void loop1() {
-  //delay(2500);
+void loop() {
+  Serial1.println("test");
+  delay(2500);
   //rotary_encoder_input.read_rotary_encoder();
 }
+
