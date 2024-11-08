555202fa1c642171bca938128f176008767e2982
Nick <niklas.braun30419@gmail.com>
2024-11-08 10:18:31 +0100

    Ported Current state of project over

--- EmbeddedSolutionPicoArduino/src/main.cpp
+++ EmbeddedSolutionPicoArduino/src/main.cpp
@@ -1,27 +1,37 @@
 #include<Arduino.h>
 #include "rotary_encoder.h"
 
-#define ROT_A 8
-#define ROT_B 9
-#define ROT_C 10
+#define ROT_A 9
+#define ROT_B 10
+#define ROT_C 11
 rotary_encoder::input rotary_encoder_input(ROT_A, ROT_B, ROT_C);
 
+void pot_interrupt() {
+  rotary_encoder_input.read_rotary_encoder();
+}
+
+
 // Setup1 is used to use the second CPU core
-void setup1() {
+void setup() {
+  // Debug Probe
+  Serial1.setRX(1);
+  Serial1.setTX(0);
+  Serial1.begin(115200);
+
+
+
   pinMode(ROT_A, INPUT_PULLUP);
   pinMode(ROT_B, INPUT_PULLUP);
   pinMode(ROT_C, INPUT_PULLUP);
-  PCICR |= B00000001;
-  PCMSK0 |= B00000111;
+
+  attachInterrupt(digitalPinToInterrupt(ROT_A), pot_interrupt, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ROT_B), pot_interrupt, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ROT_C), pot_interrupt, CHANGE);
 
   Serial.begin(115200);
 }
 
-void loop1() {
+void loop() {
   //delay(2500);
-  //read_rotary_encoder();
-}
-
-ISR(PCINT0_vect) {
-  rotary_encoder_input.read_rotary_encoder();
+  //rotary_encoder_input.read_rotary_encoder();
 }
