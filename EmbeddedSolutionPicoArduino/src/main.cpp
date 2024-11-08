a4faeccb4306d8a638b88cbe8a7d5ce927de0933
Nick <niklas.braun30419@gmail.com>
2024-11-08 10:19:52 +0100

    Make interrupts work on second core

--- EmbeddedSolutionPicoArduino/src/main.cpp
+++ EmbeddedSolutionPicoArduino/src/main.cpp
@@ -10,9 +10,17 @@ void pot_interrupt() {
   rotary_encoder_input.read_rotary_encoder();
 }
 
+void setup() {
+
+}
+
+void loop() {
+
+}
+
 
 // Setup1 is used to use the second CPU core
-void setup() {
+void setup1() {
   // Debug Probe
   Serial1.setRX(1);
   Serial1.setTX(0);
@@ -31,7 +39,7 @@ void setup() {
   Serial.begin(115200);
 }
 
-void loop() {
+void loop1() {
   //delay(2500);
   //rotary_encoder_input.read_rotary_encoder();
 }
