29952cb0d785878d902f2b3ef769e25de3eea1d9
Zuhao <yalin1998@hotmail.com>
2024-11-19 19:16:49 +0100

    Cleaner view test, with mock method of raising all tables

---
+++ Famicom/Components/Pages/Cleaner.razor.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using Famicom.Models;
+
+namespace Famicom.Components.Pages
+{
+    public partial class CleanerBase : ComponentBase
+    {
+        public bool IsCleaningMode { get; private set; }
+        public bool CanActivateCleaningMode => DateTime.Now.Hour >= 18;
+
+        private TableModel tableModel = new TableModel();
+
+        public void ToggleCleaningMode()
+        {
+            if (CanActivateCleaningMode)
+            {
+                IsCleaningMode = !IsCleaningMode;
+
+                if (IsCleaningMode)
+                {
+                    SetAllTablesToMaxHeight();
+                }
+                else
+                {
+                    ResetTablesToNormalHeight();
+                }
+            }
+        }
+
+        private void SetAllTablesToMaxHeight()
+        {
+            try
+            {
+                tableModel.UpdateAllTablesMaxHeight(true);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error setting tables to max height: {ex.Message}");
+            }
+        }
+
+        private void ResetTablesToNormalHeight()
+        {
+            try
+            {
+                tableModel.UpdateAllTablesMaxHeight(false);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error resetting tables: {ex.Message}");
+            }
+        }
+    }
+}
