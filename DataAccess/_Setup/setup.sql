be55ad82f5c268025cc8a8f08372e8242926408f
Nick <niklas.braun30419@gmail.com>
2024-11-24 18:54:31 +0100

    Add table to sql

--- DataAccess/_Setup/setup.sql
+++ DataAccess/_Setup/setup.sql
@@ -107,4 +107,8 @@ INSERT INTO APIS (A_NAME, A_CONFIG) VALUES
 ('Mock API', '{"controller": "MockTableController"}'),
 ('Linak API', '{"controller": "LinakTableController"}');
 
+INSERT INTO TABLES (T_GUID, T_NAME, T_MANUFACTURER, T_API) VALUES 
+('cd:fb:1a:53:fb:e6', 'DESK 4486', 'Linak A/S', 1);
+
+
 COMMIT;
