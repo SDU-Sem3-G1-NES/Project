b7651a25231157dfa426fb518334f432207978f3
Nick <niklas.braun30419@gmail.com>
2024-11-29 18:21:12 +0100

    Implemented TableComponent on Employee Dashboard

--- Famicom/Components/Pages/DashboardEmployee.razor.cs
+++ Famicom/Components/Pages/DashboardEmployee.razor.cs
@@ -1,4 +1,8 @@
+using Blazored.SessionStorage;
+using Famicom.Models;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using SharedModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +12,23 @@ namespace Famicom.Components.Pages
 {
     public partial class DashboardEmployee : ComponentBase
     {
+        [Inject] ISessionStorageService _sessionStorage { get; set; } = default!;
+        private TableModel _tableModel { get; set; } = null!;
+        public TableComponent _tableComponent { get; set; } = null!;
+        private ITable _table { get; set; } = null!; 
+        private int userId { get; set; } = 0;
+
+        protected override async Task OnInitializedAsync()
+        {
+            _tableModel = new TableModel();
+            await base.OnInitializedAsync();
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            userId = await _sessionStorage.GetItemAsync<int>("UserId");
+            _table = _tableModel.GetTable(userId)!;
+            StateHasChanged();
+        }
     }
 }
