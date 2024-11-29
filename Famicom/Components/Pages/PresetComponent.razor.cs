06dd31db1de40722393b124db936a03a9017087a
DovydasGan <dogan23@student.sdu.dk>
2024-11-29 17:32:54 +0100

    Finished preset component

--- Famicom/Components/Pages/PresetComponent.razor.cs
+++ Famicom/Components/Pages/PresetComponent.razor.cs
@@ -2,55 +2,65 @@ using Famicom.Models;
 using SharedModels;
 using Microsoft.AspNetCore.Components;
 using MudBlazor;
-using Blazored.SessionStorage;
 using System.Diagnostics;
-using System.Linq.Expressions;
+using TableController;
+using Blazored.SessionStorage;
+
 
 namespace Famicom.Components.Pages
 {
     public partial class PresetComponent : ComponentBase
     {
         public enum OverlayMode{ None, Add, Edit }
-        private List<string> iconOptions = new List<string>
+        private Dictionary<string, string> iconOptions = new Dictionary<string, string>
         {
-            "fa-user",
-            "fa-star",
-            "fa-heart"
+            { "fa-user", "user" },
+            { "fa-star", "star" },
+            { "fa-heart", "heart" },
+            { "fa-arrow-up", "arrow up" },
+            { "fa-arrow-down", "arrow down" }
         };
         private OverlayMode currentOverlayMode = OverlayMode.None;
         private PresetsModel? presetsModel { get; set; }
+        private TableModel? tableModel { get; set; }
         public List<Presets>? userPresets { get; set; }
-
-        [CascadingParameter]
-        public string? Email { get; set; }
-
-        [CascadingParameter]
-        public int? UserId { get; set; }
         private int PresetId { get; set; }
         private string? PresetName { get; set; }
         private int PresetHeight { get; set; }
         private string? PresetIcon { get; set; }
         private string? ErrorMessage { get; set; }
+        private int UserId { get; set; }
+
+        [Inject] 
+        ISessionStorageService SessionStorage { get; set; } = default!;
+
+        [Inject]
+        IHttpClientFactory ClientFactory { get; set; } = default!;
 
         [Inject]
         public ISnackbar Snackbar { get; set; } = default!;
-        [Inject]
-        private ISessionStorageService? SessionStorage { get; set; }
 
-        private bool _isInitialized;
-
-        protected override async void OnInitialized()
+        protected override async Task OnInitializedAsync()
         {
             try
             {
-                presetsModel = new PresetsModel();
-                userPresets = await Task.Run(() => presetsModel.GetAllPresets((int)UserId!));
-                StateHasChanged();
+                
+                tableModel = new TableModel();
+                presetsModel = new PresetsModel(ClientFactory);
+
             }
             catch (Exception ex)
             {
                 ErrorMessage = ex.Message;
             }
+            await base.OnInitializedAsync();
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            UserId = await SessionStorage.GetItemAsync<int>("UserId");
+            userPresets = await Task.Run(() => presetsModel!.GetAllPresets(UserId));
+            StateHasChanged();
         }
         public void OpenOverlay(OverlayMode mode, int presetId = 0)
         {
@@ -75,11 +85,27 @@ namespace Famicom.Components.Pages
             PresetIcon = string.Empty;
             ErrorMessage = null;
         }
-        private async Task SelectPreset(int presetId)
+        private async Task SelectPreset(string presetName, int presetHeight)
         {
-            // This is where the selected preset would be applied to the table
-            // Does it go to the tablecontroller or straight to the database?
-            await Task.CompletedTask;
+            try
+            {
+                Snackbar.Add($"Applying preset {presetName}...", Severity.Info);
+                var table = tableModel!.GetTable(UserId);
+                if (table == null)
+                {
+                    Snackbar.Add("Table not found for the user", Severity.Error);
+                    return;
+                }
+                var task = await presetsModel!.SetPresetHeight(presetHeight, table.GUID);
+                var severity = task.Status == TableStatus.Success ? Severity.Success : Severity.Error;
+                Snackbar.Add($"Status: {task.Status}, Message: {task.Message}", severity);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                Snackbar.Add("An error occurred while selecting the preset", Severity.Error);
+                return;
+            }
         }
         private async Task AddPreset()
         {
@@ -88,13 +114,21 @@ namespace Famicom.Components.Pages
                 ErrorMessage = "Preset name is required.";
                 return;
             }
-
-            if (PresetHeight <= 0)
+            if(PresetName.Length >15)
             {
-                ErrorMessage = "Preset height must be greater than zero.";
+                ErrorMessage = "The preset name must be no more than 15 characters long.";
                 return;
             }
-
+            if (PresetHeight < 680)
+            {
+                ErrorMessage = "Preset height must be more than or equal to 680mm.";
+                return;
+            }
+            if (PresetHeight > 1320)
+            {
+                ErrorMessage = "Preset height must be less than or equal to 1320mm.";
+                return;
+            }
             if (string.IsNullOrEmpty(PresetIcon))
             {
                 ErrorMessage = "Preset icon is required.";
@@ -103,7 +137,7 @@ namespace Famicom.Components.Pages
 
             try
             {
-                await Task.Run(() => presetsModel?.AddPreset(PresetName, (int)UserId!, PresetHeight, "{}", PresetIcon));
+                await Task.Run(() => presetsModel?.AddPreset(PresetName, UserId, PresetHeight, "{}", PresetIcon));
                 ErrorMessage = null;
                 Snackbar.Add("Preset added successfully", Severity.Success);
                 userPresets = await Task.Run(() => presetsModel?.GetAllPresets((int)UserId!));
@@ -124,13 +158,21 @@ namespace Famicom.Components.Pages
                 ErrorMessage = "Preset name is required.";
                 return;
             }
-
-            if (PresetHeight <= 0)
+            if(PresetName.Length >15)
             {
-                ErrorMessage = "Preset height must be greater than zero.";
+                ErrorMessage = "The preset name must be no more than 15 characters long.";
                 return;
             }
-
+            if (PresetHeight < 680)
+            {
+                ErrorMessage = "Preset height must be more than or equal to 680 mm.";
+                return;
+            }
+            if (PresetHeight > 1320)
+            {
+                ErrorMessage = "Preset height must be less than or equal to 1320 mm.";
+                return;
+            }
             if (string.IsNullOrEmpty(PresetIcon))
             {
                 ErrorMessage = "Preset icon is required.";
@@ -139,10 +181,10 @@ namespace Famicom.Components.Pages
 
             try
             {
-                await Task.Run(() => presetsModel?.EditPreset(PresetId, PresetName, (int)UserId!, PresetHeight, "{}", PresetIcon));
+                await Task.Run(() => presetsModel?.EditPreset(PresetId, PresetName, UserId, PresetHeight, "{}", PresetIcon));
                 ErrorMessage = null;
                 Snackbar.Add("Preset edited successfully", Severity.Success);
-                userPresets = await Task.Run(() => presetsModel?.GetAllPresets((int)UserId!));
+                userPresets = await Task.Run(() => presetsModel?.GetAllPresets(UserId));
             }
             catch (Exception e)
             {
