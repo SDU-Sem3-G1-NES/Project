c63278c706fc68f3d3208242f0cdd06307330963
DovydasGan <dogan23@student.sdu.dk>
2024-12-01 01:12:00 +0100

    You can set the height, but I had trouble retrieving the height of the table

--- Famicom/Components/Pages/Dashboard.razor.cs
+++ Famicom/Components/Pages/Dashboard.razor.cs
@@ -18,15 +18,28 @@ namespace Famicom.Components.Pages
         [CascadingParameter]
         public int? UserId { get; set; }
 
+        public ITable _table { get; set; } = null!;
+        private TableModel tableModel { get; set; } = null!;
+        private int userId { get; set; }
         [Inject] NavigationManager? Navigation { get; set; }
+        [Inject] IHttpClientFactory clientFactory { get; set; } = default!;
+        [Inject] ISessionStorageService sessionStorage { get; set; } = default!;
 
         protected override async Task OnInitializedAsync()
         {
+            tableModel = new TableModel(clientFactory);
             if(Navigation != null && Navigation.Uri.Replace(Navigation.BaseUri, "/") == "/") {
                 Navigation.NavigateTo("/Dashboard");
                 return;
             }
             await base.OnInitializedAsync();
+        }
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            
+            userId = await sessionStorage.GetItemAsync<int>("UserId");
+            _table = tableModel.GetTable(userId)!;
+            StateHasChanged();
         }
     }
 }
--- Famicom/Components/Pages/TableComponent.razor.cs
+++ Famicom/Components/Pages/TableComponent.razor.cs
@@ -1,29 +1,63 @@
 ﻿using Famicom.Models;
+using MudBlazor;
 using SharedModels;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Components;
+using TableController;
 
 namespace Famicom.Components.Pages
 {
     public partial class TableComponent : ComponentBase
     {
-        
-        public string TableImageUrl { get; set; } = "";
-
         private TableModel? tableModel { get; set; }
-
+        private int tableHeight { get; set; } = 1000; // mock
+        private int tempHeight { get; set; } = 1000; // mock
+        private string? ErrorMessage { get; set; }
+        
         [Parameter]
         public required ITable Table { get; set; }
 
+        [Inject]
+        public ISnackbar Snackbar { get; set; } = default!;
 
-        public void MoveTableUp()
+        [Inject]
+        IHttpClientFactory ClientFactory { get; set; } = default!;
+
+        protected override void OnInitialized()
         {
-            Debug.WriteLine("Table moved up");
+            try
+            {
+                tableModel = new TableModel(ClientFactory);
+            }
+            catch (Exception ex)
+            {
+                ErrorMessage = ex.Message;
+            }
         }
 
-        public void MoveTableDown()
+        private void AdjustTableHeight(int height)
         {
-            Debug.WriteLine("Table moved down");
+            tempHeight += height;
+            StateHasChanged();
+        }
+
+        private async Task SetTableHeight()
+        {
+            try
+            {
+                Snackbar.Add($"Setting height to {(decimal)tempHeight/10} cm...", Severity.Info);
+                var task = await tableModel!.SetTableHeight(tempHeight, Table.GUID);
+                var severity = task.Status == TableStatus.Success ? Severity.Success : Severity.Error;
+                Snackbar.Add($"Status: {task.Status}, Message: {task.Message}", severity);
+                tableHeight = await tableModel.GetTableHeight(Table.GUID);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                Snackbar.Add("An error occurred while setting the height", Severity.Error);
+                return;
+            }
+
         }
     }
 }
